ee0211c686763876b04b0edd7843fdc4001a056d
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2022-12-15 18:02:48 +0300

    faet: add tag main

--- online-store/src/pages/MainPage/index.tsx
+++ online-store/src/pages/MainPage/index.tsx
@@ -3,9 +3,9 @@ import { SectionProducts } from "../../components/SectionProducts";
 
 export const MainPage: FC = () => {
   return (
-    <>
+    <main>
       <h2>Main page</h2>
       <SectionProducts />
-    </>
+    </main>
   );
 };
