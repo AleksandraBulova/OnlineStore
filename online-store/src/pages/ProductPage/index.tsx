29d73c506d3047af07007ed823354077e7a54ed7
RussianBoy64 <kozlachkovvd@gmail.com>
2022-12-18 23:41:20 +0400

    feat: add styles to products section header

--- online-store/src/pages/ProductPage/index.tsx
+++ online-store/src/pages/ProductPage/index.tsx
@@ -1,22 +1,21 @@
-import { FC } from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { ProductCard } from "../../components/ProductCard";
-import { RootState } from "../../redux/store";
-import { IProduct } from "../../types";
+import { FC } from "react"
+import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import { ProductCard } from "../../components/ProductCard"
+import { RootState } from "../../redux/store"
+import { IProduct } from "../../types"
 
 export const ProductPage: FC = () => {
-  const { id } = useParams();
-  const products = useSelector((state: RootState) => state.products.products);
+  const { id } = useParams()
+  const products = useSelector((state: RootState) => state.products.products)
 
   return (
     <>
       <h2>Product page</h2>
       <ProductCard
-        product={
-          products.find((product) => product.id === Number(id)) as IProduct
-        }
+        product={products.find((product) => product.id === Number(id)) as IProduct}
+        view="gorizontal"
       />
     </>
-  );
-};
+  )
+}
