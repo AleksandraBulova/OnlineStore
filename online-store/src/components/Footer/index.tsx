33f1853dc0ab9822ee16a8c6cba3cbe8b678f98e
RussianBoy64 <kozlachkovvd@gmail.com>
2022-12-19 22:32:42 +0400

    feat: add footer component and styles

--- online-store/src/components/Footer/index.tsx
+++ online-store/src/components/Footer/index.tsx
@@ -1,9 +1,40 @@
-import { FC } from "react";
+import { FC } from "react"
+
+import styles from "./styles.module.scss"
 
 export const Footer: FC = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className={styles.footer}>
+      <div className={styles.authorInfo}>
+        <div className={styles.authorInfo__item}>
+          <a href="https://rs.school/js/">
+            <img src="https://rs.school/images/rs_school_js.svg" alt="RS School" />
+          </a>
+        </div>
+        <div className={styles.authorInfo__item}>
+          <span>2022</span>
+        </div>
+        <div className={styles.authorInfo__item}>
+          <div className={styles.github}>
+            <i className={`fa-brands fa-github ${styles.github__icon}`}></i>
+            <a
+              className={styles.github__link}
+              href="https://github.com/AleksandraBulova"
+              target="_blank"
+            >
+              AleksandraBulova
+            </a>
+            <span> / </span>
+            <a
+              className={styles.github__link}
+              href="https://github.com/RussianBoy64"
+              target="_blank"
+            >
+              RussianBoy64
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
-  );
-};
+  )
+}
